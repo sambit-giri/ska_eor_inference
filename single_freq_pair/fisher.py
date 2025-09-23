2cfe56b1f97fa71142668519a866d8b93ae1e372
AbinashKumarShaw <abinashkumarshaw@gmail.com>
2025-09-23 11:08:04 -0700

    Added diagonal and single_freq_pair MAPS analysis

---
+++ single_freq_pair/fisher.py
@@ -0,0 +1,180 @@
+import numpy as np
+import h5py
+import tqdm
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from matplotlib.colors import SymLogNorm   
+import corner
+import os, sys
+import glob
+
+# Folder where the data is stored
+# ddir = '/data/cluster/agorce/SKA_chapter_simulations/'
+ddir = '../../../../data/' # This folder can be created inside the repository folder. It will be ignored during the git commit.
+# File with fiducial lightcone
+file = ddir+'Lightcone_FID_400_Samples.h5'
+
+statname = 'smt_redmaps100'
+
+# cosmology
+h = 0.6774
+
+# Astro params
+params = ['ION_Tvir_MIN','R_BUBBLE_MAX','HII_EFF_FACTOR']
+Fisher_Param = ['$T_{Vir}$','$R_{Max}$','$\zeta$']
+nparams = len(params)
+# parameter values for fiducial simulation
+fid = [pow(10,4.7),15,30]
+# parameter range for derivatives
+delta_params = [pow(10,4.740362689494244)-pow(10,4.653212513775344), 10, 10]
+
+# Power spectrum params
+nbins = 10
+
+# simulation files
+files = [ddir+'Lightcone_FID_400_Samples.h5']
+[files.extend(
+    [f'{ddir}Lightcone_{p}_400_Samples_Plus.h5', f'{ddir}Lightcone_{p}_400_Samples_Minus.h5']) for p in params]
+#print(files)
+
+# Read h5py file for metadata
+with h5py.File(files[0], 'r') as f:
+    frequencies = f['frequencies'][...]
+    redshifts = f['redshifts'][...]
+    box_length = float(f['box_length'][0])/h  # Mpc
+    box_dim = int(f['ngrid'][0])
+    n_samp = int(f['nrealisations'][0])
+nfreq = frequencies.size
+print(f'Lightcone runs from z={redshifts.min():.2f} to z = {redshifts.max():.2f}.')
+
+
+nfreq_full = nfreq
+red_factor = 8
+nfreq = int(nfreq_full/red_factor)
+
+nu = np.array([np.mean(frequencies[i:i+red_factor]) for i in range(0,nfreq_full, red_factor)])
+zz = 1420.406/nu - 1.
+
+# container dict: keys will be e.g. 'PS_param_1_plus', 'PS_fid', etc.
+MAPS_data = {}
+keys = []
+count = 0
+for fpath in tqdm.tqdm(files):
+
+    key = os.path.basename(fpath)[:-3].replace('Lightcone_', '').replace('_400_Samples', '')
+    keys.append(key)
+    try:
+        with h5py.File(fpath, 'r') as f:
+            ell = f['bins'][...]
+            #mask = ks < k_nyquist
+            MAPS_data[key] = {
+                'maps_clean':  f[statname+'_clean'][:],  # ps of clean cosmological signal
+                'ell':  f['bins'][:],
+            }
+            if 'FID' in key:
+                MAPS_data[key].update({
+                    'maps_noise':  f[statname+'_noise'][:],  # ps of noise
+                    'maps_obs':  f[statname+'_obs'][:],  # ps of smoothed (noise + cosmological signal)
+                })
+    except KeyError:
+        MAPS_data[key] = {}
+        MAPS_data[key]['maps_clean'] = np.zeros((n_samp, nbins, nfreq, nfreq))
+        MAPS_data[key]['maps_noise'] = np.zeros((n_samp, nbins, nfreq, nfreq))
+        MAPS_data[key]['maps_obs'] = np.zeros((n_samp, nbins, nfreq, nfreq))
+        MAPS_data[key]['ell'] = np.zeros(nbins)
+        count += 1
+#print(keys)
+# now you can refer to, e.g. PS_data['PS_R_BUBBLE_MAX_plus']['PS'] 
+# and PS_data['PS_R_BUBBLE_MAX_plus']['ks'] etc.
+print(f'Found {len(MAPS_data)} {statname} datasets, {count} missing.')
+
+# Example: check shapes
+for name, dat in MAPS_data.items():
+    print(f"{name:25s} → MAPS {dat['maps_clean'].shape},  ell {dat['ell'].shape}")
+
+
+
+# Compute the finite‐difference derivatives for each k‐bin & sample
+# from the statistics derived from the clean signal
+dMAPS = {}
+MAPS_derivs = np.zeros((len(params), n_samp, nbins, nfreq, nfreq))
+for ip, param in enumerate(params):
+    dMAPS[param] = (MAPS_data[f'{param}_Plus']['maps_clean'] - MAPS_data[f'{param}_Minus']['maps_clean']) / delta_params[ip]
+    MAPS_derivs[ip] = (MAPS_data[f'{param}_Plus']['maps_clean'] - MAPS_data[f'{param}_Minus']['maps_clean']) / delta_params[ip]
+#ks = MAPS_data[f'{param}_Plus']['ks']
+
+print(dMAPS['R_BUBBLE_MAX'].shape)
+print(MAPS_derivs.shape)
+
+# Now computing the covariance of the MAPS using 400 realizations of the signal
+
+fid_data =  MAPS_data['FID']['maps_obs'][:]
+ell = MAPS_data['FID']['ell'][:]
+
+
+ell_lim = 8 # number of ell bins, starting from the smallest
+
+fid_data = fid_data[:, :ell_lim, :, :]
+ell = ell[:ell_lim]
+MAPS_derivs = MAPS_derivs[:, :, :ell_lim, : , :]
+
+Nell = len(ell)
+
+stdx = np.std(fid_data, axis=0)
+
+fid_data = fid_data/stdx[None,:,:,:]
+MAPS_derivs = MAPS_derivs/stdx[None, None,:,:,:]
+
+freq1 = int(nfreq//2) # channel number 1
+freq2 = int(freq1 -1) # channel number 2
+
+derv_ = np.mean(MAPS_derivs[:,:,:,freq1,freq2], axis=1)
+data_ = fid_data[:,:,freq1,freq2]
+errcov = np.cov(data_, rowvar=False)
+inv_cov = np.linalg.inv(errcov)
+x = np.matmul(derv_, inv_cov)
+#print(x.shape)
+F = np.matmul(x, derv_.T)
+
+im = plt.imshow(errcov, origin='lower', cmap='coolwarm', vmax=1., vmin=-1.)
+plt.colorbar(im)
+plt.gcf().set_size_inches(6,6)
+plt.savefig('covar_diag_100_AAstar.png', format='png', dpi=300, bbox_inches='tight')
+plt.close()
+plt.clf()
+
+print(f"Condition number for the corresponding covariance matrix is: {np.log10(np.linalg.cond(errcov)):.4f}")
+
+param_cov = np.linalg.inv(F)
+print(param_cov)
+
+np.savetxt('param_cov_100_AAstar.txt', param_cov, fmt='%.6e', delimiter='\t', newline='\n')
+
+############### Checking the convergence of the Fisher analysis ################
+# Number of realisations to look at when checking convergence
+samples = np.arange(20, n_samp+5, 5)
+
+# Initialising Fishers 
+MAPS_Fisher = np.zeros((samples.size, nparams, nparams))
+MAPS_Fisher_Inv = np.zeros((samples.size, nparams, nparams))
+
+# Calculating the Fisher matrix for a given sample of derivatives 
+for k, sample_size in enumerate(samples):
+    derv_ = np.mean(MAPS_derivs[:,:sample_size,:,freq1,freq2], axis=1)
+    data_ = fid_data[:sample_size,:,freq1,freq2]
+    errcov = np.cov(data_, rowvar=False)
+    inv_cov = np.linalg.inv(errcov)
+    x = np.matmul(derv_, inv_cov)
+    #print(x.shape)
+    MAPS_Fisher[k] += np.matmul(x, derv_.T)
+    MAPS_Fisher_Inv[k] = np.linalg.inv(MAPS_Fisher[k])
+
+plt.figure(figsize=(12, 4))
+for i, param in enumerate(params):
+    plt.plot(samples, [np.diagonal(MAPS_Fisher_Inv[k])[i]-np.diagonal(MAPS_Fisher_Inv[-1])[i] for k in range(samples.size)], marker='.', label=param)
+plt.legend()
+plt.ylabel(r'$\sigma^2_{ii}$')
+plt.xlabel('Number of samples')
+plt.title('Convergence of the Parameters Variances')
+plt.savefig('convg_100_AAstar.png', format='png', dpi=300, bbox_inches='tight')
+plt.close()
